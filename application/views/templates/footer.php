3a3461bc8a4b79bd1e229c63e3372ac676c7468a
Quentin Diaferia <q.diaferia@gmail.com>
2017-05-09 13:30:13 +0200

    Link to JQuery fixed

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,7 +4,7 @@
 		</div>
 		<div class="col-sm-3"></div>
 
-		<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-3.2.1.min.js"); ?>"></script>
 		<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 	</body>
 </html>
